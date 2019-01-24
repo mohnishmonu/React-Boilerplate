5c622ac27a0a87f455528c836abb2252b03d3aa9
Mohnish <mohnish34@gmail.com>
2019-01-24 15:44:08 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const title = "The React Webpack Babel Setup";
-
-ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
