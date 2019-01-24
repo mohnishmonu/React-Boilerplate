765234dcd153aee4f025b14b6b37a22b4142cec0
Mohnish <mohnish34@gmail.com>
2019-01-24 15:32:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "The React Webpack Babel Setup";
+
+ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
